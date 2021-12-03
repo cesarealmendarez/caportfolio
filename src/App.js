78b496141830e1ac05399d95570867a33a4d1813
Cesar <cesarealmendarez@gmail.com>
2021-12-02 16:14:55 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <html>
+      <head>
+        <title>Cesar Almendarez</title>
+      </head>
+      <div>
+        <h1>Cesar Almendarez</h1>
+
+        <ul style={{paddingLeft: 0}}>
+          <li style={{display: "inline", marginRight: 10}}><a target="#" href="https://github.com/cesarealmendarez">GitHub</a></li>
+          <li style={{display: "inline", marginRight: 10}}><a target="#" href="http://linkedin.com/in/cesar-almendarez-1b7191189">LinkedIn</a></li>
+          <li style={{display: "inline"}}><a target="#" href="https://cesarealmendarez.github.io/caportfolio/">Porfolio Website</a></li>
+        </ul>
+
+        <h3>I make stuff that works sometimes</h3>
+
+        <p>Like this ASL Interpreter using ML and computer vision:</p>
+        <a target="#" href="https://share.streamlit.io/cesarealmendarez/deep_asl_streamlit/app.py">DeepASL on Streamlit</a>
+      </div>
+    </html>
   );
 }
 
