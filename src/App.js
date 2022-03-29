3fd8abf0e12b0ca195ad0dd33186b61fae52c9e5
Cesar <cesarealmendarez@gmail.com>
2022-03-29 12:59:09 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 function App() {
   return (
     <html>
-      <head>
-        <title>CAPORTFOLIO</title>
-      </head>
-      <div>
-        <h1 style={{fontFamily: 'Courier New'}}>Cesar Almendarez</h1>
+        <head>
+            <title>CAPORTFOLIO</title>
+        </head>
+        <div style={{alignContent: 'center', alignItems: 'center', justifyContent: 'center', justifyItems: 'center', textAlign: 'center'}}>
+            <h1 style={{fontFamily: 'Courier New'}}>Cesar Almendarez</h1>
 
-        <ul style={{paddingLeft: 0}}>
-          <li style={{display: "inline", marginRight: 10}}><a target="#" href="https://github.com/cesarealmendarez">GitHub</a></li>
-          <li style={{display: "inline", marginRight: 10}}><a target="#" href="http://linkedin.com/in/cesar-almendarez-1b7191189">LinkedIn</a></li>
-          <li style={{display: "inline"}}><a target="#" href="https://cesarealmendarez.github.io/caportfolio/">Porfolio Website</a></li>
-          <li style={{display: "inline"}}><a target="#" href="https://docs.google.com/document/d/1hr0LxKA7Bhq8POI7TNEQjE1KnEonh1hi50fFTNop1ck/edit?usp=sharing">Resume</a></li>
-        </ul>
+            <ul style={{paddingLeft: 0}}>
+            <li style={{display: "inline", marginRight: 10}}><a target="#" href="https://github.com/cesarealmendarez">GitHub</a></li>
+            <li style={{display: "inline", marginRight: 10}}><a target="#" href="http://linkedin.com/in/cesar-almendarez-1b7191189">LinkedIn</a></li>
+            <li style={{display: "inline", marginRight: 10}}><a target="#" href="https://docs.google.com/document/d/1gOlWXDIF459vwN1_SwSTT7xqEGfVsQZx/edit?usp=sharing&ouid=106413919968155890770&rtpof=true&sd=true">Resume</a></li>
+            </ul>
 
-        <h3>I make software that works sometimes</h3>
+            <h3>I make software that works most of the time!</h3>
 
-        <p>Like this ASL Interpreter using Convolutional Neural Nets and and OpenCV:</p>
-        <a target="#" href="https://share.streamlit.io/cesarealmendarez/deep_asl_streamlit/app.py">DeepASL on Streamlit</a>
-      </div>
+            <p>Like this ASL interpreter using Convolutional Neural Networks and OpenCV:</p>
+            <img style={{width: 500, height: 250, objectFit: 'cover', borderRadius: 5, marginBottom: 10}} src="https://user-images.githubusercontent.com/89669770/160533990-ae71afe4-67f4-4d21-93b4-8bf65fba739f.gif"/>
+            <br></br>
+            <a style={{marginRight: 10}} target="#" href="https://github.com/cesarealmendarez/DeepASL">DeepASL on GitHub</a>
+            <a target="#" href="https://share.streamlit.io/cesarealmendarez/deep_asl_streamlit/app.py">DeepASL on Streamlit</a>
+        </div>
     </html>
   );
 }
